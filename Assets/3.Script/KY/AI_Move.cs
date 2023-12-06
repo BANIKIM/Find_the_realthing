43df4d975859d09ddd3fd3fc12a9f9921372744e
Kwan <janyouns@gmail.com>
2023-12-06 14:45:50 +0900

    State Change
    Arrive to Target
    20% Stop
    80% Move

--- Assets/3.Script/KY/AI_Move.cs
+++ Assets/3.Script/KY/AI_Move.cs
@@ -1,48 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AI_Move : MonoBehaviour
 {
-    [Header("Å¸°Ù")]
     [SerializeField] private Transform[] TargetPos;
     private Transform Target = null;
-    private Animator ani;
-    
 
-    [SerializeField] private float MoveSpeed = 3f;    
+    private Animator ani;
+    private NavMeshAgent navMesh;
+
+    private bool isMove = false;
+    private float Distance = 3f;          // Å¸°Ù°úÀÇ °Å¸®
+
     private void Start()
     {
         TryGetComponent<Animator>(out ani);
+        TryGetComponent<NavMeshAgent>(out navMesh);
     }
 
     public void Move()
     {
-        if (Target == null)
+        if (Target != null && isMove)
         {
-            TargetSetting();
-        }
-        if (Target != null)
-        {
-            transform.position += transform.forward * Time.deltaTime * MoveSpeed;
+            navMesh.SetDestination(Target.position);
 
-            if (isArrive())
+            if (isArrive() && isMove)
             {
-                Target = null;
+                ani.SetBool("isRun", false);
+                isMove = false;
+
+                int Temp = Random.Range(0, 5);
+                if (Temp.Equals(4)) Invoke("ResetTarget", Temp + 2);
+                else ResetTarget();
             }
         }
+        else if (Target == null && !isMove) TargetSetting();
     }
 
+    private void ResetTarget()
+    {
+        Target = null;
+    }
     private void TargetSetting()
     {
         Target = TargetPos[Random.Range(0, TargetPos.Length)];           // ·£´ý Å¸°Ù¼³Á¤
-        transform.forward = Target.position - transform.position;        // Å¸°Ù ¹Ù¶óº¸±â
-        ani.SetTrigger("Walk");
+
+        ani.SetBool("isRun", true);
+        isMove = true;
     }
 
     private bool isArrive()        // Å¸°Ù ÁÖº¯¿¡ ÀÖ´ÂÁö
     {
-        return (Vector3.Magnitude(transform.position - Target.position) < 1) ? true : false;
+        return (Vector3.Magnitude(transform.position - Target.position) < Distance) ? true : false;
     }
 
 }
