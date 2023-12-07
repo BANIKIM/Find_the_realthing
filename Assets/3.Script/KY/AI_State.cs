514bf41afa41ed9d87944a5e2c694b73de6c4ba9
BANIKIM <72877425+BANIKIM@users.noreply.github.com>
2023-12-07 09:50:55 +0900

    [Fix] AI 수정했습니다.

--- Assets/3.Script/KY/AI_State.cs
+++ Assets/3.Script/KY/AI_State.cs
@@ -1,18 +1,23 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AI_State : MonoBehaviour
 {
-    private AI_Move ai_Move;
+    public AI_Move ai_Move;
     public Animator animator;
 
     private bool isDie = false;     // 죽었는지
+    //네비메쉬
+    private NavMeshAgent nav;
 
     private void Awake()
     {
         TryGetComponent<AI_Move>(out ai_Move);
-       
-        animator = transform.GetChild(0).GetComponent<Animator>();
-       // TryGetComponent<Animator>(out animator);
+        //ai_Move = transform.GetChild(0).GetComponent<AI_Move>();//1207 mhkim 추가
+        //animator = transform.GetChild(0).GetComponent<Animator>();
+         TryGetComponent<Animator>(out animator);
+        TryGetComponent(out nav);
+
     }
 
     private void Update()
@@ -25,20 +30,41 @@ public class AI_State : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Attack") && !isDie)
+        if (other.gameObject.CompareTag("Attack") && !isDie)
         {
+            Debug.Log("dddd");
             OnDie();    // 플레이어에게 맞았을 때 OnDie 메소드 호출
         }
-            
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Attack") && !isDie)
+        {
+            Debug.Log("스테이");
+        }
     }
 
     private void OnDie()
     {
         isDie = true;
         animator.SetTrigger("isDie");
+        //재윤님 코드
+        transform.position = new Vector3(transform.position.x, 0, transform.position.z); // 죽을 때 y축이 바뀌면 땅에 묻혀서 y축 고정
         Rigidbody rig = GetComponent<Rigidbody>();
         CapsuleCollider cap = GetComponent<CapsuleCollider>();
+        NavmeshStop();
         rig.isKinematic = true;
         cap.isTrigger = true;
+    }
+
+    private void NavmeshStop()//재윤님 메서드
+    {
+        // don't slide
+        nav.isStopped = true;
+        nav.updatePosition = false;
+        nav.updateRotation = false;
+        nav.velocity = Vector3.zero;
     }
 }
