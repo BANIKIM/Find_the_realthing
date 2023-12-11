09d87f00d3b6e4ed67759f30035d372b1f72bb48
Kwan <>
2023-12-11 10:09:36 +0900

    1211 AudioManager & Option
    AudioManager & Option Prefab
    MainBGM, Stab1~3, ClickSFX1~3

--- Assets/3.Script/KY/Option.cs
+++ Assets/3.Script/KY/Option.cs
@@ -14,6 +14,9 @@ public enum Channel
 public class Option : MonoBehaviour
 {
     public static Option instance = null;
+   
+    [SerializeField] private GameObject OptionObj;
+    private bool isPause = false;
 
     [Header("¿Àµð¿À ¹Í¼­")]
     public AudioMixer audioMixer;
@@ -32,6 +35,22 @@ public class Option : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPause)
+            {
+                OptionObj.SetActive(false);
+                isPause = false;
+            }
+            else
+            {
+                OptionObj.SetActive(true);
+                isPause = true;
+            }
+        }
+    }
     public void Master_SetVolume()
     {
         audioMixer.SetFloat("Master", Audio_Sliders[(int)Channel.Master].value);
